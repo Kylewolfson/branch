37eb06fcc3698e5b8bf0e32f42ec5e68f69f6cf7
Kevin Deganos and Kyle Wolfson <kyle@>
2016-03-24 10:17:23 -0700

    Fixed hide and show of rides

--- js/park-scripts.js
+++ js/park-scripts.js
@@ -4,19 +4,22 @@ $(function(){
     var rider = parseInt($("#inches").val());
 
     if (rider <= 40) {
-      $(".rides").removeClass("hide");
+      $(".rides").show();
+      // $(".rides").removeClass("hide");
       $("#merry, #teacups").addClass("highlight");
       $("#coaster").removeClass("highlight");
     }
 
     else if (rider >= 70) {
       $(".rides").show();
+      // $(".rides").removeClass("hide");
       $("#merry, #coaster").addClass("highlight");
       $("#teacups").removeClass("highlight");
     }
 
     else {
       $(".rides").show();
+      // $(".rides").removeClass("hide");
       $("#merry, #teacups, #coaster").addClass("highlight");
     }
   });
